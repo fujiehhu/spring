1f36ec6361d9323424486367742e37ff1ede698d
fujiehhu <1163844624@qq.com>
2020-08-03 22:26:16 +0800

    2020年8月2日19点09分

--- src/main/java/test/testSpring.java
+++ src/main/java/test/testSpring.java
@@ -12,8 +12,11 @@ public class testSpring {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring.xml");
 
+        // 从 context 中取出我们的 Bean，而不是用 new MessageServiceImpl() 这种方式
         MessageServiceImpl messageService = context.getBean(MessageServiceImpl.class);
+
         System.out.println(messageService.getMessage());
+        // System.out.println(((MessageServiceImpl)context.getBean("messageService")).getMessage());
 
     }
 }
